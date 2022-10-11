2b8ac266914c1b31cc4199111a5df0f3d4782080
ThomasCode92 <thomascode92@hotmail.com>
2022-10-11 22:45:58 +0200

    Creating the Basic Express App

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
 
+// Activate EJS view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render('home');
 });
 
 app.listen(3000, () => {
