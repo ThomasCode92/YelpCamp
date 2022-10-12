eadeaeefb9ccc3d71c29ff7073547294c79f2a14
ThomasCode92 <thomascode92@hotmail.com>
2022-10-12 21:05:49 +0200

    Campground New & Create

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true })); // Parse incoming request bodies
+
 app.use('/campgrounds', campgroundRoutes);
 
 mongoose
--- routes/campgrounds.routes.js
+++ routes/campgrounds.routes.js
@@ -9,10 +9,23 @@ router.get('/', async (req, res) => {
   res.render('campgrounds/index', { campgrounds });
 });
 
+router.get('/new', (req, res) => {
+  res.render('campgrounds/new-campground');
+});
+
 router.get('/:id', async (req, res) => {
   const campgroundId = req.params.id;
   const campground = await Campground.findById(campgroundId);
   res.render('campgrounds/campground-details', { campground });
+});
+
+router.post('/', async (req, res) => {
+  const { title, location } = req.body.campground;
+
+  const campground = new Campground({ title, location });
+  await campground.save();
+
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 module.exports = router;
