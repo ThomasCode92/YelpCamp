149bb94ed922321c3a592e12ef1eec821003ddb3
ThomasCode92 <thomascode92@hotmail.com>
2022-10-12 19:24:45 +0200

    Campground Model Basics

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const path = require('path');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
+const Campground = require('./models/campground.model');
 
 const app = express();
+
+const DB_CONNECTION_STRING = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongo:27017/${process.env.DB_NAME}?authSource=admin`;
 
 // Activate EJS view engine
 app.set('view engine', 'ejs');
@@ -12,6 +17,14 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+mongoose
+  .connect(DB_CONNECTION_STRING)
+  .then(() => console.log('Connected to MongoDB'))
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Server listening on port 3000');
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
