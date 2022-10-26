d6a11f187d8d387a80ce19a27b8aac8d375c8cdc
ThomasCode92 <thomascode92@hotmail.com>
2022-10-26 12:37:10 +0200

    Creating Reviews

--- routes/reviews.routes.js
+++ routes/reviews.routes.js
@@ -2,11 +2,26 @@ const express = require('express');
 
 const catchAsync = require('../util/catchAsync');
 
+const Campground = require('../models/campground.model');
+const Review = require('../models/review.model');
+
 const router = express.Router({ mergeParams: true });
 
 router.post(
   '/',
   catchAsync(async (req, res, next) => {
+    const campgroundId = req.params.campId;
+    const { rating, text } = req.body.review;
+
+    const newReview = { body: text, rating: parseInt(rating, 10) };
+
+    const campground = await Campground.findById(campgroundId);
+    const review = new Review(newReview);
+
+    campground.reviews.push(review);
+
+    await Promise.all([review.save(), campground.save()]);
+
     res.json({ message: 'Review created successfully' });
   })
 );
