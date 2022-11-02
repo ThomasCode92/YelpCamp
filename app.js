4a46ff1926668568f4a72275fa31b00346b6f42b
ThomasCode92 <thomascode92@hotmail.com>
2022-11-02 19:16:48 +0100

    Configuring Session

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const expressSession = require('express-session');
 
+const createSessionConfig = require('./config/session');
 const ExpressError = require('./util/ExpressError');
 
 const campgroundsRoutes = require('./routes/campgrounds.routes');
 const reviewsRoutes = require('./routes/reviews.routes');
 
 const app = express();
+const sessionConfig = createSessionConfig();
 
 // Activate EJS view engine
 app.set('view engine', 'ejs');
@@ -18,6 +21,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public')); // Serve static files (e.g. CSS files)
 app.use(express.urlencoded({ extended: true })); // Parse incoming request bodies
 app.use(express.json());
+
+app.use(expressSession(sessionConfig)); // Create the Express Session
 
 app.use(methodOverride('_method')); // Override POST requests having _method in the query string
 
