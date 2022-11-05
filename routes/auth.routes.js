60bf1dee52840a469c4735e31272739091eddc90
ThomasCode92 <thomascode92@hotmail.com>
2022-11-05 09:15:10 +0100

    Adding Reviews & Auth Controller

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,81 +1,25 @@
 const express = require('express');
 
-const { flashDataToSession } = require('../util/session-flash');
-
 const authenticate = require('../middleware/authenticate');
 
-const User = require('../models/user.model');
+const {
+  getRegister,
+  postRegister,
+  getLogin,
+  postLogin,
+  logout,
+} = require('../controllers/auth.controller');
 
 const router = express.Router();
 
-router.get('/register', (req, res, next) => {
-  res.render('auth/register');
-});
+router.get('/register', getRegister);
 
-router.post('/register', async (req, res, next) => {
-  const { username, email, password } = req.body.auth;
+router.post('/register', postRegister);
 
-  const user = new User({ username, email });
+router.get('/login', getLogin);
 
-  try {
-    const registeredUser = await User.register(user, password);
+router.post('/login', authenticate, postLogin);
 
-    req.login(registeredUser, error => {
-      if (!error) {
-        const flashData = {
-          status: 'success',
-          message: 'Welcome to YelpCamp!',
-        };
-
-        flashDataToSession(req, flashData, () => {
-          res.redirect('/campgrounds');
-        });
-      } else {
-        next(error);
-      }
-    });
-  } catch (error) {
-    const flashData = {
-      status: 'error',
-      message: `${error.message}!`,
-    };
-
-    flashDataToSession(req, flashData, () => {
-      res.redirect('/auth/register');
-    });
-  }
-});
-
-router.get('/login', (req, res, next) => {
-  res.render('auth/login');
-});
-
-router.post('/login', authenticate, (req, res, next) => {
-  const redirectUrl = req.session.returnToUrl || '/campgrounds';
-
-  const flashData = {
-    status: 'success',
-    message: 'Welcome back to YelpCamp!',
-  };
-
-  flashDataToSession(req, flashData, () => {
-    res.redirect(redirectUrl);
-  });
-});
-
-router.get('/logout', (req, res, next) => {
-  req.logout(error => {
-    if (error) return next(error);
-
-    const flashData = {
-      status: 'success',
-      message: 'Goodbye!',
-    };
-
-    return flashDataToSession(req, flashData, () => {
-      res.redirect('/campgrounds');
-    });
-  });
-});
+router.get('/logout', logout);
 
 module.exports = router;
