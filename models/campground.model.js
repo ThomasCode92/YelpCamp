98ecaee0db7fcce9766b32f444db4d73913823c4
ThomasCode92 <thomascode92@hotmail.com>
2022-11-07 20:03:09 +0100

    Adding a Thumbnail Virtual Property

--- models/campground.model.js
+++ models/campground.model.js
@@ -4,21 +4,28 @@ const Review = require('./review.model');
 
 const { Schema, model } = mongoose;
 
+function createThumbnail() {
+  return this.url.replace('/upload', '/upload/w_200');
+}
+
 async function deleteReviews(doc) {
   if (!doc) return;
   await Review.deleteMany({ _id: { $in: doc.reviews } });
 }
+
+const ImageSchema = new Schema({ url: String, filename: String });
 
 const CampgroundSchema = new Schema({
   title: String,
   price: Number,
   description: String,
   location: String,
-  images: [{ url: String, filename: String }],
+  images: [ImageSchema],
   reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }],
   author: { type: Schema.Types.ObjectId, ref: 'User' },
 });
 
+ImageSchema.virtual('thumbnail').get(createThumbnail);
 CampgroundSchema.post('findOneAndDelete', deleteReviews);
 
 module.exports = model('Campround', CampgroundSchema);
