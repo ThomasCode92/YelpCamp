bff958807dcd9e70fd32fb4ed7b4e7fc48201272
ThomasCode92 <thomascode92@hotmail.com>
2022-11-24 20:39:21 +0100

    Centering the Map on a Campground

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -2,14 +2,20 @@
 
 mapboxgl.accessToken = mapToken;
 
+const { coordinates } = JSON.parse(geometry);
+
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
-  center: [-74.5, 40],
+  center: coordinates,
   zoom: 9,
   projection: 'globe',
 });
 
+const marker = new mapboxgl.Marker();
+
 map.on('style.load', () => {
   map.setFog({});
 });
+
+marker.setLngLat(coordinates).addTo(map);
