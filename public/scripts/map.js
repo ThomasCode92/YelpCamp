dd706a7ad59dc3c78a42120b6622c9640a3a67c4
ThomasCode92 <thomascode92@hotmail.com>
2022-11-24 21:19:16 +0100

    Customizing Map Popup

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -2,20 +2,29 @@
 
 mapboxgl.accessToken = mapToken;
 
-const { coordinates } = JSON.parse(geometry);
+const campgroundData = JSON.parse(campground);
 
 const map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
-  center: coordinates,
+  style: 'mapbox://styles/mapbox/light-v10',
+  center: campgroundData.geometry.coordinates,
   zoom: 9,
   projection: 'globe',
 });
 
 const marker = new mapboxgl.Marker();
+const popup = new mapboxgl.Popup({ offset: 25 });
 
 map.on('style.load', () => {
   map.setFog({});
 });
 
-marker.setLngLat(coordinates).addTo(map);
+popup.setHTML(`
+  <h3>${campgroundData.title}</h3>
+  <p>${campgroundData.location}</p>
+`);
+
+marker
+  .setLngLat(campgroundData.geometry.coordinates)
+  .setPopup(popup)
+  .addTo(map);
